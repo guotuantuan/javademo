17217e845b171bae4ddfa963c57e02969c8e1a0a
guotuantuan <guotuantuan@58.com>
2021-03-24 10:52:54 +0800

    提交代码

--- src/main/java/com/gself/study/jExercise/algorithmDemo/AgDemo.java
+++ src/main/java/com/gself/study/jExercise/algorithmDemo/AgDemo.java
@@ -4,26 +4,42 @@ public class AgDemo {
     public static void main(String[] args) {
         AgDemo agDemo = new AgDemo();
         int[] arr = {5, 4, 3, 2, 1};
-        agDemo.bubbleSort(arr);
+        agDemo.selectSort(arr);
         for (int i : arr) {
             System.out.println(i);
         }
     }
-    public void bubbleSort(int[]arr){
-        if(arr==null||arr.length<2){
+
+    public void selectSort(int[] arr) {
+        if (arr == null || arr.length < 2) {
             return;
         }
-        for(int i=arr.length-1;i>0;i--){
-            for(int j=0;j<i;j++){
-                if(arr[j]>arr[j+1]){
-                    swap(arr,j,j+1);
+        for (int i = 0; i < arr.length-1; i++) {
+            int index = i;
+            for(int j = i+1;j<arr.length;j++){
+                if(arr[j]<arr[i]){
+                    index = j;
+                }
+            }
+            swap(arr,index,i);
+        }
+    }
+
+    public void bubbleSort(int[] arr) {
+        if (arr == null || arr.length < 2) {
+            return;
+        }
+        for (int i = arr.length - 1; i > 0; i--) {
+            for (int j = 0; j < i; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    swap(arr, j, j + 1);
                 }
             }
 
         }
     }
 
-    public void swap(int []arr,int x,int y){
+    public void swap(int[] arr, int x, int y) {
         int temp = arr[x];
         arr[x] = arr[y];
         arr[y] = temp;
